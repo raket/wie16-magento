a7f5ce4d206e052504536cb4924feafc71f40379
Oskar Lind <mail@oskarlind.com>
2016-06-17 00:19:38 +0200

    Shipping notice module

---
+++ Backend/ShippingNotice/Block/Notice.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Oskarlind\ShippingNotice\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Notice extends Template {
+    protected $_helper;
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = [],
+        \Oskarlind\ShippingNotice\Helper\Data $helper
+    ) {
+        parent::__construct($context, $data);
+        $this->_helper = $helper;
+    }
+
+    public function getNotice() {
+        return $this->_helper->getNotice();
+    }
+}
---
+++ Backend/ShippingNotice/Helper/Data.php
@@ -0,0 +1,39 @@
+<?php
+namespace Oskarlind\ShippingNotice\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper
+{
+
+    protected $_scopeConfig;
+    CONST ENABLE = 'amasty_helloworld/general/enable';
+    CONST BLOCK_LABEL = 'amasty_helloworld/general/block_label';
+    CONST TEXT_ALIGN = 'amasty_helloworld/general/text_align';
+
+    public function __construct(\Magento\Framework\App\Helper\Context $context,
+                                \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig)
+    {
+        parent::__construct($context);
+        $this->_scopeConfig = $scopeConfig;
+    }
+
+    public function getEnable()
+    {
+        return $this->_scopeConfig->getValue('oskarlind_shippingnotice/general/enable');
+    }
+
+    public function getOutputText(){
+        return $this->_scopeConfig->getValue('oskarlind_shippingnotice/general/output_text');
+    }
+
+    public function getHoursRemaining() {
+        $midnight = strtotime("tomorrow 00:00:00");
+        return floor(($midnight - time()) / 3600);
+    }
+
+    public function getNotice() {
+        if($this->getEnable()) {
+            return $this->getOutputText() . $this->getHoursRemaining();
+        }
+        return "";
+    }
+}
